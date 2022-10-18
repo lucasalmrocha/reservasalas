bbf4804228a440fe78676fa7f3a0d5f1a72c9ceb
Lucas Almeida Rocha <65246663+lucasalmrocha@users.noreply.github.com>
2022-10-18 17:51:17 -0300

    Delete ReservaSala directory

--- ReservaSala/reservasalas/main/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField, DateField, TimeField
-from wtforms.validators import DataRequired, InputRequired, Length
-
-class SearchForm(FlaskForm):
-    searched=StringField('Searched', validators=[DataRequired()])
-    submit = SubmitField('Pesquisar')
--- ReservaSala/run.py
+++
@@ -1,6 +0,0 @@
-from reservasalas import create_app
-
-app = create_app('config.py')
-
-if __name__ == '__main__':
-	app.run(debug=True)
